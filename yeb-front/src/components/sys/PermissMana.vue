9c5009e46b5a0de30bdc6fd8f7d73a3ea4e52165
shizuwei <shizuwei99@gmail.com>
2021-05-26 10:20:10 +0800

    角色功能实现

--- yeb-front/src/components/sys/PermissMana.vue
+++ yeb-front/src/components/sys/PermissMana.vue
@@ -4,8 +4,8 @@
       <el-input size="small" placeholder="请输入角色英文名" v-model="role.name">
         <template slot="prepend">ROLE_</template>
       </el-input>
-      <el-input size="small" v-model="role.nameZh" placeholder="请输入角色中文名"/>
-      <el-button size="small" type="primary" icon="el-icon-plus">添加角色</el-button>
+      <el-input size="small" v-model="role.nameZh" @keydown.enter.native="doAddRole" placeholder="请输入角色中文名"/>
+      <el-button size="small" type="primary" icon="el-icon-plus" @click="doAddRole" >添加角色</el-button>
     </div>
 
     <div class="pmsMain">
@@ -15,11 +15,14 @@
             <div slot="header" class="clearfix">
               <span>可访问资源</span>
               <el-button style="float: right; padding: 3px 0; color:#ff0000" type="text"
-                         icon="el-icon-delete"></el-button>
+                         icon="el-icon-delete" @click="doDeleteRole(item)"></el-button>
             </div>
             <div>
-              <el-tree :data="allMenus" :props="defaultProps" show-checkbox
+              <el-tree :data="allMenus"
+                       :props="defaultProps"
+                       show-checkbox
                        ref="tree"
+                       :key="index"
                        :default-checked-keys="selectMenus" node-key="id" >
 
               </el-tree>
@@ -61,6 +64,38 @@ export default {
   },
   methods: {
 
+    doDeleteRole(data) {
+      this.$confirm('此操作将永久删除角色[' + data.nameZh + '], 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.deleteRequest('/system/basic/permiss/role/' + data.id).then(resp => {
+          if (resp) {
+            this.initRoles();
+          }
+        });
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
+    },
+
+    doAddRole() {
+      if (this.role.name && this.role.nameZh) {
+        this.postRequest('/system/basic/permiss/role', this.role).then(resp => {
+          if (resp) {
+            this.initRoles();
+            this.role.name = '';
+            this.role.nameZh = '';
+          }
+        });
+      }else
+        this.$message.error("所有字段不能为空！");
+    },
+
     cancelUpdate() {
       this.activeName = '-1';
     },
@@ -74,7 +109,6 @@ export default {
       });
       this.putRequest(url).then(resp => {
         if (resp) {
-          this.initRoles();
           this.activeName = '-1';
         }
       });
