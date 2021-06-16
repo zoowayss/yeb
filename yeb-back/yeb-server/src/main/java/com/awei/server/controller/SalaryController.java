7cde9bfc8f613d4ff7a902f9f2aa7626641ffe50
shi zuwei <shizuwei99@gmail.com>
2021-06-16 11:44:10 +0800

    工资账套实现

--- yeb-back/yeb-server/src/main/java/com/awei/server/controller/SalaryController.java
+++ yeb-back/yeb-server/src/main/java/com/awei/server/controller/SalaryController.java
@@ -1,20 +1,62 @@
 package com.awei.server.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.awei.server.pojo.RespBean;
+import com.awei.server.pojo.Salary;
+import com.awei.server.service.ISalaryService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author shizuwei
  * @since 2021-03-09
  */
 @RestController
-@RequestMapping("/salary")
+@RequestMapping("/salary/sob")
 public class SalaryController {
+    @Autowired
+    private ISalaryService salaryService;
+
+    @ApiOperation("获取所有工资账套")
+    @GetMapping("/")
+    public List<Salary> getAllSalaries() {
+        return salaryService.list();
+    }
+
+    @ApiOperation("添加工资账套")
+    @PostMapping("/")
+    public RespBean addSalary(@RequestBody Salary salary) {
+        salary.setCreateDate(LocalDateTime.now());
+        if (salaryService.save(salary)) {
+            return RespBean.success("添加成功！");
+        }
+        return RespBean.error("添加失败！");
+    }
+
+    @ApiOperation("删除工资账套")
+    @DeleteMapping("/{id}")
+    public RespBean deleteSalary(@PathVariable Integer id) {
+        if (salaryService.removeById(id)) {
+            return RespBean.success("删除成功！");
+        }
+        return RespBean.error("删除失败！");
+    }
+
+    @ApiOperation("更新工资账套")
+    @PutMapping("/")
+    public RespBean updateSalary(@RequestBody Salary salary) {
+        if (salaryService.updateById(salary)) {
+            return RespBean.success("更新成功！");
+        }
+        return RespBean.error("删除失败！");
+    }
 
 }
